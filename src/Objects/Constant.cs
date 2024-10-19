09fded5bc1f51280eda997b9049536d0f385127d
Rodrigo Ferreira <rodrigofxxx04@gmail.com>
2024-10-19 01:09:09 +0100

    Reset de key (Não pode ser mais usada) e LICENSE.

--- src/Objects/Constant.cs
+++ src/Objects/Constant.cs
@@ -1,8 +1,23 @@
-﻿namespace IP_Address_Lookup.src.Objects
+﻿/*
+ * MIT License
+ * 
+ * This software is provided under the MIT License, a permissive license that allows for the use, copying, modification, 
+ * merging, publishing, distribution, sublicensing, and sale of copies of the software, as long as the original 
+ * permission and disclaimer are included in all copies or substantial portions of the software.
+ * 
+ * For more details about the MIT License and its terms, please refer to:
+ * https://github.com/rodriaum/IP-Address-Lookup?tab=MIT-1-ov-file#readme
+ * 
+ * This project is an IP address lookup system developed in C#. Contributions are welcome!
+ * 
+ * Copyright (c) 2024 Rodrigo Ferreira
+ */
+
+namespace IP_Address_Lookup.src.Objects
 {
     internal class Constant
     {
-        public static readonly string PROXY_CHECK_KEY = "6970ks-844208-715231-i770y6";
+        public static readonly string PROXY_CHECK_KEY = "";
 
         public static readonly string PROXY_CHECK_V2_API = "https://proxycheck.io/v2/{0}?key={1}?vpn=1&asn=1";
         public static readonly string IPIFY_API = "https://api.ipify.org/";
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,18 @@
+/*
+ * MIT License
+ * 
+ * This software is provided under the MIT License, a permissive license that allows for the use, copying, modification, 
+ * merging, publishing, distribution, sublicensing, and sale of copies of the software, as long as the original 
+ * permission and disclaimer are included in all copies or substantial portions of the software.
+ * 
+ * For more details about the MIT License and its terms, please refer to:
+ * https://github.com/rodriaum/IP-Address-Lookup?tab=MIT-1-ov-file#readme
+ * 
+ * This project is an IP address lookup system developed in C#. Contributions are welcome!
+ * 
+ * Copyright (c) 2024 Rodrigo Ferreira
+ */
+
 namespace IP_Address_Lookup.src
 {
     internal static class Program
