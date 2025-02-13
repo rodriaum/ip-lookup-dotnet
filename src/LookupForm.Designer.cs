2e5704856d491469abb68c8f6ce57210f8449e81
Rodriaum <rodrigofxxx04@gmail.com>
2025-02-13 23:36:54 +0000

    Changed font family

--- src/LookupForm.Designer.cs
+++ src/LookupForm.Designer.cs
@@ -21,6 +21,12 @@ namespace IP_Address_Lookup
     partial class LookupForm
     {
         /// <summary>
+        ///  Project font variable.
+        /// </summary>
+        private FontFamily font = new FontFamily("Barlow");
+
+        /// 
+        /// <summary>
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
@@ -61,7 +67,7 @@ namespace IP_Address_Lookup
             addressCaptionLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             addressCaptionLabel.AutoSize = true;
             addressCaptionLabel.BackColor = Color.FromArgb(20, 21, 40);
-            addressCaptionLabel.Font = new Font("Segoe UI Variable Display", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            addressCaptionLabel.Font = new Font(this.font, 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             addressCaptionLabel.ForeColor = SystemColors.ScrollBar;
             addressCaptionLabel.Location = new Point(210, 350);
             addressCaptionLabel.Name = "addressCaptionLabel";
@@ -73,7 +79,8 @@ namespace IP_Address_Lookup
             // 
             titleLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             titleLabel.AutoSize = true;
-            titleLabel.Font = new Font("Segoe UI Variable Small Semibol", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            //Segoe UI Variable Small Semibo
+            titleLabel.Font = new Font(this.font, 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             titleLabel.ForeColor = Color.White;
             titleLabel.Location = new Point(330, 28);
             titleLabel.Name = "titleLabel";
@@ -97,7 +104,7 @@ namespace IP_Address_Lookup
             resultCustomListBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             resultCustomListBox.BackColor = Color.FromArgb(15, 19, 32);
             resultCustomListBox.BorderStyle = BorderStyle.None;
-            resultCustomListBox.Font = new Font("Segoe UI Symbol", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            resultCustomListBox.Font = new Font(this.font, 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             resultCustomListBox.ForeColor = Color.White;
             resultCustomListBox.FormattingEnabled = true;
             resultCustomListBox.ItemHeight = 20;
@@ -112,7 +119,7 @@ namespace IP_Address_Lookup
             addressTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             addressTextBox.BackColor = Color.FromArgb(15, 19, 32);
             addressTextBox.BorderStyle = BorderStyle.None;
-            addressTextBox.Font = new Font("Segoe UI Variable Display", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            addressTextBox.Font = new Font(this.font, 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
             addressTextBox.ForeColor = Color.White;
             addressTextBox.Location = new Point(200, 380);
             addressTextBox.Multiline = true;
@@ -127,7 +134,7 @@ namespace IP_Address_Lookup
             statusLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             statusLabel.AutoSize = true;
             statusLabel.BackColor = Color.FromArgb(20, 21, 40);
-            statusLabel.Font = new Font("Candara", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            statusLabel.Font = new Font(this.font, 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             statusLabel.ForeColor = SystemColors.ScrollBar;
             statusLabel.Location = new Point(12, 78);
             statusLabel.Name = "statusLabel";
